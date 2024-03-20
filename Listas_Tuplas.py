b5bbb2b3b644090cb498e54fd50c1266d2c992af
JuanBecerra07 <162609964+JuanBecerra07@users.noreply.github.com>
2024-03-20 00:27:22 +0000

    Listas_Tuplas

--- Listas_Tuplas.py
+++ Listas_Tuplas.py
@@ -1,24 +1,60 @@
 """
 hoy vamos a ver listas y tuplas
-* listas 
+* listas []
     conjunto de elementos con muchos tipo de datos num strings, boolean etc etc 
     pueden ser homogenas 
     pueden heterogeneas 
     Mutables
+    se pueden concardenar 
+    con lista+ por ejemplo solo agregando un + luego del nombre de la variable 
     se crean al escvribirlos como un conjunto  ls =[1,2,l,6,7]] y también se pueden indexar y operar
        -- metodos de las listas 
-         se pueden operar columbas de una tabla es un lenguaje básicas 
+         se pueden operar columbas de una tabla es un lenguaje básicas
 
+       -- Metodo   :
+         append
+         clear 
+         copy  
+         count
+         extend
+         index
+         index con value, start y stop 
+         insert en un posicion algo 
+         pop borra el [ultimo elemento 
+         pop(1) borra por index( necesita remove con el  index)
+         remove no es lo mismo que el pop, el remove necesita un elemento para eliminar, hay que especificar el valor 
+         reverse 
+         sort ordena de forma alfa num de forma acendente o descendente, necesita tener siempre el mismo tipo de dato en las listas  
 
-* Tuplas 
+* Tuplas ()
     se crean cuando se necesita que los datos jamás cambien 
     se crean pero con parentesis 
     pueden ser homogenas 
     pueden heterogeneas 
     se pueden indexar pero no se pueden cambiar no son mutábles 
+    se pueden concardenar 
+    
+    solo da el conteo y el index de la info   
+      ---Metodos
+        Count 
+        Index
+        
+
 
 * Diccionarios 
     tiene un llave y un valor 
     ID - nombre
+
+
+
+
+* Operadores
+   Aritméticos
+   Algebráicos
+   lógicos
+   Negación 
+   Relacionales(compraciones)
+   AND, OR, NOT (funciona igual que las puertas lógicas)
+
 
 """
--- testing.py
+++ testing.py
@@ -11,23 +11,6 @@ print(planet[1:4:2])
 """"""
 
 """
-hoy vamos a ver listas y tuplas
-* listas 
-    conjunto de elementos con muchos tipo de datos num strings, boolean etc etc 
-    pueden ser homogenas 
-    pueden heterogeneas 
-    Mutables
-    se crean al escvribirlos como un conjunto  ls =[1,2,l,6,7]] y también se pueden indexar y operar
-       -- metodos de las listas 
-         se pueden operar columbas de una tabla es un lenguaje básicas 
-
-
-* Tuplas 
-    se crean cuando se necesita que los datos jamás cambien 
-    se crean pero con parentesis 
-    pueden ser homogenas 
-    pueden heterogeneas 
-    se pueden indexar pero no se pueden cambiar no son mutábles 
-       
+acordarse de ir borrando lo que ya no haya por que no tiene un garbage colector 
       
 """
